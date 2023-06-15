158d211da1f77bf94e66b9b0e1937e38af08fa2e
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-06-14 22:05:05 -0500

    hello

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import style from './App.module.scss'
 
 function App() {
 
   return (
-    <>
+    <div className={style['app']}>
     <h1>Hola mundo desde tarea 3 otra vez</h1>
-    </>
+    </div>
   )
 }
 
